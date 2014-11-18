357b2a6ce75052c677dc3503d0bcd99525e8d111
MaorG <maor@slatescience.com>
2014-11-18 22:28:42 +0200

    - virtual classifier class

--- include/Classifier.h
+++ include/Classifier.h
@@ -1,40 +1,18 @@
 #pragma once
 #include "common.h"
 #include "Model.h"
-#include "Entity.h"
-#include "floatfann.h"
-#include "fann_cpp.h"
 
 class Classifier
 {
 protected:
-	std::vector<std::vector <float>> trainingInput;
-	std::vector<std::vector<float>> trainingOutput;
-	int m_inputVectorSize;
-	int m_sampleSize;
-	struct fann* ann = NULL;
-
-	std::vector<struct fann*> fannBattery;
-
-	std::vector <float> convertEntityToInputVector(Entity * entity, int sampleSize);
-	std::vector <float> convertModelToInputVector(Model * model, int sampleSize); 
-	void prepareNNBattery();
-
-	fann_type ** Classifier::convertTrainingInputToVectors();
-	fann_type ** convertTrainingOutputToVectors(int digitIndex);
-
 public:
-	void setInputVectorSize(int inputVectorSize_) { m_inputVectorSize = inputVectorSize_; };
-	void setSampleSize(int sampleSize_) { m_sampleSize = sampleSize_; };
-	void prepareTrainingData(std::vector<Model*> * inputModels, int sampleSize);
-	Model* GetPreprocessedModel(Model *model, int sampleSize);
-	void train();
 
-	// todo: these belong in another class of classifier
-	void trainBattery();
-	void classifyBattery(Model * model);
-	void testBattery(float ratio);
-	void classify(Model * model);
+	virtual ~Classifier(){};
+	virtual void prepareTrainingData(std::vector<Model*> * inputModels, int sampleSize) = 0;	
+	virtual void trainBattery() = 0;
+	virtual void classifyBattery(Model * model);
+	virtual void testBattery(float ratio);
+	virtual void classify(Model * model);
 
 };
 
---
+++ include/ClassifierNNSingle.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "common.h"
+#include "Model.h"
+#include "Entity.h"
+#include "floatfann.h"
+#include "fann_cpp.h"
+
+class Classifier
+{
+protected:
+	std::vector<std::vector <float>> trainingInput;
+	std::vector<std::vector<float>> trainingOutput;
+	int m_inputVectorSize;
+	int m_sampleSize;
+	struct fann* ann = NULL;
+
+	std::vector<struct fann*> fannBattery;
+
+	std::vector <float> convertEntityToInputVector(Entity * entity, int sampleSize);
+	std::vector <float> convertModelToInputVector(Model * model, int sampleSize); 
+	void prepareNNBattery();
+
+	fann_type ** Classifier::convertTrainingInputToVectors();
+	fann_type ** convertTrainingOutputToVectors(int digitIndex);
+
+public:
+	void setInputVectorSize(int inputVectorSize_) { m_inputVectorSize = inputVectorSize_; };
+	void setSampleSize(int sampleSize_) { m_sampleSize = sampleSize_; };
+	void prepareTrainingData(std::vector<Model*> * inputModels, int sampleSize);
+	Model* GetPreprocessedModel(Model *model, int sampleSize);
+	void train();
+
+	// todo: these belong in another class of classifier
+	void trainBattery();
+	void classifyBattery(Model * model);
+	void testBattery(float ratio);
+	void classify(Model * model);
+
+};
+
